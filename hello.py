beff7ce49a61a3dc432a0a3f30acd6fc5f873224
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-01-05 13:01:41 +0530

    third commit

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 print("Hello world")
 print("Login successful")
+
+Username = input("Enter username: ")
+password = input("Enter password: ")
+if Username == "Pavan" and password == "12345":
+    print("Login successful")
+else:
+    print("Login again")
+
