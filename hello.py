be67e8afc57477843b16b9e50ce7b97eef15de76
pindiproluphani2023-ctrl <pindiprolu.phani2023@vitstudent.ac.in>
2026-01-19 12:45:42 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
 print("Hello world")
 print("Login successful")
 
-Username = input("Enter username: ")
-password = input("Enter password: ")
-if Username == "Pavan" and password == "12345":
-    print("Login successful")
-else:
-    print("Login again")
 
